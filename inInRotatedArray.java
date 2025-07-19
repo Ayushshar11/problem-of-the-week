f0e293b7fe047eefef40d36bead43820ea25d5f3
Ayush Sharma <ayushsharmabros@gmail.com>
2025-07-19 12:02:00 +0530

    Added comments

--- inInRotatedArray.java
+++ inInRotatedArray.java
@@ -1,3 +1,5 @@
+// Min in Sort rotated Array [week 1 -> Question 1]
+// Approach Binary Search 
 import java.util.Scanner;
 class MinInRotatedArray{
     public static void main(String[] args){
@@ -21,3 +23,30 @@ class MinInRotatedArray{
         System.out.println(arr[low]);
     }
 }
+
+// Appraoch 2 Linear Search 
+
+/*import java.util.Scanner;
+
+public class MinInRotatedArray_Linear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println(min);
+    }
+}*/
